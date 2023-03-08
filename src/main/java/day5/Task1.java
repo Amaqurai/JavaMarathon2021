fcd6202cbd429db2392edba3bc50e6486dda51f3
Tokarev Ilya <nb.raipo@gmail.com>
2023-03-08 16:15:35 +0500

    completed the tasks of the fifth day

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,38 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car newCar = new Car();
+        newCar.setModel("Renault");
+        newCar.setColor("Red");
+        newCar.setYear(2020);
 
+        System.out.println(newCar.getModel());
+        System.out.println(newCar.getColor());
+        System.out.println(newCar.getYear());
+    }
+
+    public static class Car {
+        private String model;
+        private String color;
+        private int year;
+
+        public void setModel(String modelName) {
+            model = modelName;
+        }
+        public String getModel() {
+            return model;
+        }
+        public void setColor(String colorName) {
+            color = colorName;
+        }
+        public String getColor() {
+            return color;
+        }
+        public void setYear(int yearIssue) {
+            year = yearIssue;
+        }
+        public int getYear() {
+            return year;
+        }
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,35 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike newMotorbike = new Motorbike("Moto", "red", 154);
 
+        System.out.println(newMotorbike.getModel());
+        System.out.println(newMotorbike.getColor());
+        System.out.println(newMotorbike.getYear());
     }
+}
+
+class Motorbike {
+    String model;
+    String color;
+    int year;
+
+    public Motorbike(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
 }
