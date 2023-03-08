2a3579957d46d67685e6dd54bac9e8ab695c7a89
Tokarev Ilya <nb.raipo@gmail.com>
2023-03-08 14:33:57 +0500

    Add new option. Search max line and viev at consol

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -2,10 +2,12 @@ package day4;
 
 public class Task3 {
     public static void main(String[] args) {
+
         int m = 12; //Количество строк
         int n = 8; // Количество столбцов
 
         int[][] array = new int[m][n];
+        int[] sumMemory = new int[m]; //Вспомогательная матрица хранения сумм строк
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[j].length; j++) {
@@ -17,10 +19,18 @@ public class Task3 {
             int sumLine = 0;
             for (int j = 0; j < array[j].length; j++) {
                 sumLine += array[i][j];
-                System.out.print(array[i][j] + " ");
             }
-            System.out.println("// сумма - " + sumLine);
+            sumMemory[i] = sumLine;
         }
 
+        int maxValueIndex = 0;
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < sumMemory.length; i++) {
+            if (sumMemory[i] >= maxValue) {
+                maxValue = sumMemory[i];
+                maxValueIndex = i;
+            }
+        }
+        System.out.println("Index line with maximum sum: " + maxValueIndex);
     }
 }
