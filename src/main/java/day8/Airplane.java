41473e2a65da424d83208c685b760a74b64eb739
Tokarev Ilya <nb.raipo@gmail.com>
2023-03-12 18:57:57 +0500

    Completed the tasks of the eight day

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,54 @@
+package day8;
+public class Airplane {
+    private String manufacturer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel;
+
+    public Airplane(String manufacturer, int year, int length, int weight) {
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void info() {
+        System.out.printf("Изготовитель: %s, " +
+                "год выпуска: %d, " +
+                "длина: %d, вес: %d, " +
+                "количество топлива в баке: %d\n",
+                manufacturer, year, length, weight, getFuel());
+    }
+    public void fillUp (int n) {
+        setFuel(getFuel()+n);
+    }
+
+    public String toString (){
+        return "Изготовитель: " + manufacturer +
+                ", год выпуска: " + year +
+                ", длина: " + length +
+                ", вес: " + weight +
+                ", количество топлива в баке: " +getFuel();
+    }
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,26 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        var str = "";
+        for (int i = 0; i <= 20_000; i++) {
+            String str2 = Integer.toString(i);
+            str = str.concat(str2).concat(" ");
+        }
+        System.out.println(str);
+        long stopTime = System.currentTimeMillis();
 
+
+        long startTime1 = System.currentTimeMillis();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= 20_000; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+        long stopTime1 = System.currentTimeMillis();
+
+        System.out.println("Длительность работы конкатенации в мс: " + (stopTime - startTime));
+        System.out.println("Длительность работы стринг билдера в мс: " + (stopTime1 - startTime1));
     }
 }
