e6e429195615aecc067e05e04d169cedd710710f
Tokarev Ilya <nb.raipo@gmail.com>
2023-03-16 12:29:51 +0500

    Completed the tasks of the nine day

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,11 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Student student = new Student("Fox", "philosophy");
+        Teacher teacher = new Teacher("Skally", "chemistry");
+        System.out.println(student.getGroupName());
+        student.printInfo();
+        System.out.println(teacher.getSubjectName());
+        teacher.printInfo();
     }
 }
