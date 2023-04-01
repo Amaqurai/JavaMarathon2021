6f6ba3911fd9e995d7efb961f47fd8fcc052e090
Tokarev Ilya <nb.raipo@gmail.com>
2023-04-01 20:08:00 +0500

    Completed the tasks of the sixteen day

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,39 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("day16Task1.txt");
+        printResult(file);
+    }
 
+    public static void printResult(File file){
+
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String [] array = line.split(" ");
+
+            int sumDigits = 0;
+            int countDigits = 0;
+
+            for (String s: array) {
+                sumDigits += Integer.parseInt(s);
+                countDigits++;
+            }
+
+            double result = (double) sumDigits/countDigits;
+            //double resultCut = (double) ((int) (result * 1000)) / 1000;
+            System.out.print(result + "-->");
+            System.out.printf(Locale.ENGLISH, "%.3f", result);
+
+        } catch (FileNotFoundException e){
+            System.out.println("Файл не найден");
+        }
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,65 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        final int MAX_VALUE = 1000;
+        File file1 = new File("file1.txt");
+        File file2 = new File("file2.txt");
+        try {
+            PrintWriter printWriter = new PrintWriter(file1);
+            int randomDigit;
+            for (int i = 0; i < MAX_VALUE; i++) {
+                randomDigit = (int) (Math.random() * MAX_VALUE);
+                printWriter.print(randomDigit + " ");
+            }
+            printWriter.close();
 
+            printWriter = new PrintWriter(file2);
+            Scanner scanner = new Scanner(file1);
+            String line = scanner.nextLine();
+            String[] array = line.split(" ");
+
+            int sumDigits = 0;
+            double average;
+            final int LOT_OF_NUMBERS = 20;
+            for (int i = 0; i < MAX_VALUE; i++) {
+                if (i % LOT_OF_NUMBERS == 0 && i != 0) {
+                    average = (double) sumDigits / LOT_OF_NUMBERS;
+                    printWriter.print(average + " ");
+                    sumDigits = 0;
+                }
+                sumDigits += Integer.parseInt(array[i]);
+            }
+            printWriter.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        printResult(file1);
+
+    }
+
+    public static void printResult(File file) {
+
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] array = line.split(" ");
+            double dubSumDigits = 0;
+            for (int i = 0; i < array.length; i++) {
+                dubSumDigits = Double.parseDouble(array[i]);
+            }
+            int intSumDigits = (int) dubSumDigits;
+            System.out.println(intSumDigits);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
     }
 }
